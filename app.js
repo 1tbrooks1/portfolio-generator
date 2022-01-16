4288f55de55db9689703a86da238747d6cf111d5
tbrooks <tbrooks321@yahoo.com>
2022-01-15 22:01:50 -0500

    used inquirer to ask question and receive answer

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+/* const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const pageHTML = generatePage(name, github);
 
@@ -11,8 +8,17 @@ fs.writeFile('index.html', pageHTML, err => {
     if (err) throw err;
 
     console.log('Portfolio complete! Checkout out index.html to see the output!')
-});
+}); */
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
 
 
